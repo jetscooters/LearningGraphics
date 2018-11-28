00a5b8d494bc641f21587a607c4eb2b692e5fbba
David Silva <david.silva.25812@gmail.com>
2018-11-27 19:02:28 -0600

    Add project files.

---
+++ Engine/Actor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Actor
+    {
+        int posX;
+        int posY;
+
+        int sizeX;
+        int sizeY;
+
+        public Actor(int _posX, int _posY, int _sizeX, int _sizeY)
+        {
+            posX = _posX;
+            posY = _posY;
+
+            sizeX = _sizeX;
+            sizeY = _sizeY;
+        }
+
+        public void Move(int x, int y)
+        {
+            posX += x;
+            posY += y;
+        }
+    }
+}
